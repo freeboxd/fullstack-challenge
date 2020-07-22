8375777cbca91f36668bd9bb544581f390f73ee3
Guilherme <guilhermenunes@id.uff.br>
2020-07-21 21:31:00 -0300

    added typescript types

---
+++ fullstack-challenge-frontend/types/index.ts
@@ -0,0 +1,10 @@
+export interface UserInterface {
+  email: string,
+  password: string,
+};
+
+export interface ParticipationInterface {
+  firstName: string,
+  lastName: string,
+  participation: number|null,
+};
